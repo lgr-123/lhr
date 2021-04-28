294e0a69a877ce127e4d7e9018d549d365d78b51
linhaorui <2909753663@qq.com>
2021-04-28 14:05:46 +0800

    1

---
+++ js/index.js
@@ -0,0 +1,11 @@
+window.addEventListener('load', function() {
+
+    var audio = document.querySelector('#playy');
+
+window.addEventListener('click', function() {
+    audio.play();
+})
+    
+})
+
+
